ef817cbd431973e04ae2f22da42d9d7329c6a8a6
Michał Domino <michal.domino@yahoo.com>
2020-10-26 15:04:05 +0100

    Add getting data from api

--- dotNET-Chat-Xamarin-Forms-Client/ViewModels/LoginViewModel.cs
+++ dotNET-Chat-Xamarin-Forms-Client/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using dotNET_Chat_Xamarin_Forms_Client.Views;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using Xamarin.Forms;
 
@@ -40,6 +42,25 @@ namespace dotNET_Chat_Xamarin_Forms_Client.ViewModels
 
         private async void OnLoginClicked(object obj)
         {
+            HttpClient httpClient = new HttpClient();
+
+            //NSUrlSessis
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://192.168.1.87:49375/api/applicationusers/search"),
+                Method = HttpMethod.Get
+            };
+            //WebRequest webRequest = WebRequest.Create("http://192.168.1.87:49375/api/applicationusers/search");
+            //webRequest.Method = "GET";
+            ////webRequest.Headers =
+            var response = await httpClient.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var a = 5;
+            }
+            //WebResponse a = webRequest.GetResponse();
+            //a.Con
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
         }
